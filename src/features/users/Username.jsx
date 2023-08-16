41f0a83c82620cea4f8061bd592f9451c8c0c9b8
Joseph Beltran <profbelts@gmail.com>
2023-08-16 22:30:25 +0800

    Add redux toolkit

--- src/features/users/Username.jsx
+++ src/features/users/Username.jsx
@@ -1,3 +1,11 @@
+import { useSelector } from "react-redux";
+
 export default function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Joseph</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
